a7a9b9b93b58fe87247a0c78f63a16d477a83e2b
pangjian <pangjian1010@gmail.com>
2018-05-29 10:05:48 +0800

    init new blog

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      persistent
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      enable-resize-watcher
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      app
+      :clipped-left="clipped"
+    >
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <blog-footer></blog-footer>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import BlogFooter from './components/bone/footer.vue'
+export default {
+  data () {
+    return {
+      clipped: true,
+      drawer: true,
+      fixed: false,
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire'
+      }],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Pangjian\'s Blog'
+    }
+  },
+  name: 'App',
+  components: {
+    'BlogFooter': BlogFooter
+  }
+}
+</script>
---
+++ src/components/bone/footer.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="ml-3">
+  Made with
+  <v-icon class="red--text">favorite</v-icon>
+  by <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
+  and <a class="white--text" href="https://github.com/vwxyzjn" target="_blank">Costa Huang</a>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'BlogFooter'
+}
+</script>
+
+
+<style scoped>
+</style>
