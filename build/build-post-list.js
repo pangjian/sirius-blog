8eaff58de2a6b5c088a7a787ef02033897b335d6
pangjian <pangjian1010@gmail.com>
2018-06-08 10:19:47 +0800

    图片背景

--- build/build-post-list.js
+++ build/build-post-list.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 const matter = require('gray-matter')
+const md = require('markdown-it')()
+const _ = require('lodash')
 const blogPath = './src/blogs/'
 
 const OUTPUT_PATH = path.join(blogPath, 'post-list.json')
@@ -15,10 +17,17 @@ fs.readdir(path.join(blogPath, '_posts'), function(err, files) {
     return /\.md$/.test(post)
   }).forEach((post) => {
     let postMatter = matter.read(path.join(blogPath, '_posts', post))
-    let previewText = postMatter.content.substring(0, postMatter.content.indexOf('<!--more-->'))
-    postlist.push(Object.assign(postMatter.data, {previewText: previewText}))
+    let previewText = ''
+    if(postMatter.content.indexOf('<!--more-->') > 0) {
+      previewText = postMatter.content.substring(0, postMatter.content.indexOf('<!--more-->'))
+    } else {
+      previewText = postMatter.content.substring(0, 100) + ' ...'
+    }
+
+    postlist.push(Object.assign(postMatter.data, {previewText: previewText, previewHtml: md.render(previewText)}))
   })
-  fs.writeFile(OUTPUT_PATH, JSON.stringify(postlist), (err) => {
+  let desList = _.sortBy(postlist, 'date')
+  fs.writeFile(OUTPUT_PATH, JSON.stringify(_.reverse(desList)), (err) => {
     console.log('[build post list] DONE:', OUTPUT_PATH)
   })
 })
--- src/components/page.vue
+++ src/components/page.vue
@@ -1,6 +1,44 @@
 <template>
   <div>
-    {{ $route.params.number }}
+    <v-container >
+        <v-layout row wrap align-center>
+          <v-flex xs12 sm12 md10 offset-md1>
+            <div v-for="post in posts" :key="post.title">
+              <v-card class="my-3" hover>
+                <v-card-media
+                  class="white--text"
+                  height="350px"
+                  :src="transformStaticUrl(post.thumbnail)"
+                >
+                  <v-container fill-height fluid>
+                    <v-layout>
+                      <v-flex xs12 align-end d-flex>
+                        <span class="headline">{{ post.title }}</span>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-card-media>
+                <v-card-text>
+                  <div v-html="post.previewHtml"></div>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn icon class="red--text">
+                    <v-icon medium>fa-reddit</v-icon>
+                  </v-btn>
+                  <v-btn icon class="light-blue--text">
+                    <v-icon medium>fa-twitter</v-icon>
+                  </v-btn>
+                  <v-btn icon class="blue--text text--darken-4">
+                    <v-icon medium>fa-facebook</v-icon>
+                  </v-btn>
+                  <v-spacer></v-spacer>
+                  <v-btn flat class="blue--text">Read More</v-btn>
+                </v-card-actions>
+              </v-card>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-container>
     <div class="text-xs-center">
       <v-pagination :length="total" v-model="page"></v-pagination>
     </div>
@@ -26,11 +64,20 @@ export default {
         this.$store.dispatch('changePage', parseInt(value))
         this.$router.push({ path: '/page/' + value })
       }
+    },
+    posts: {
+      get () {
+        let pl = postList.slice((this.page - 1) * PAGE_LIMIT, this.page * PAGE_LIMIT)
+        return pl
+      },
+      set () {
+
+      }
     }
   },
   methods: {
     updatePagination () {
-      this.total = parseInt(postList.length / PAGE_LIMIT)
+      this.total = parseInt(postList.length / PAGE_LIMIT) + 1
       let page = 1
       if (this.$route.params.number) {
         if (this.$route.params.number > this.total) {
@@ -40,6 +87,16 @@ export default {
         }
       }
       this.$store.dispatch('changePage', page)
+    },
+    transformStaticUrl (url) {
+      if (!url) {
+        url = '/resources/random/material-' + Math.floor(Math.random() * 19 + 1) + '.png'
+      }
+      url = '/static' + url
+      return url
+    },
+    shinkUrl (url) {
+
     }
   },
   beforeMount () {
